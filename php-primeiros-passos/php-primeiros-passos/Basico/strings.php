8101b68aaca1a1f2eb19ef177ff3bad4b53e3017
Kelvyn <jkelvynsantos@gmail.com>
2022-04-09 22:38:01 -0300

    adicionando exercicios

---
+++ php-primeiros-passos/php-primeiros-passos/Basico/strings.php
@@ -0,0 +1,29 @@
+<?php
+
+echo 'Olá mundo';
+
+echo "Olá mundo\n";
+
+$idade = 21;
+echo 'Olá mundo! Minha idade é $idade';
+
+$idade = 21;
+echo 'Olá mundo! Minha idade é ' . $idade;
+
+$idade = 21;
+echo 'Olá mundo! Minha idade é ' . $idade . ' anos.';
+
+$idade = 21;
+echo "\nOlá mundo! Minha idade é $idade anos.\n";
+
+$idade = 21;
+echo "Olá mundo!";
+echo "Eu tenho $idade anos.";
+
+$idade = 21;
+echo "Olá mundo!";
+echo "\tEu tenho $idade anos." . PHP_EOL;
+
+$idade = 21;
+echo "Olá mundao!" . PHP_EOL;
+echo "Eu tenho $idade anos." . PHP_EOL;
